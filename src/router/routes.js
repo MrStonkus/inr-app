8a3b02344f94859ba5fd0c07d3ac7feca4da6c6a
Mr.Stonkus <mr.valdas.stonkus@gmail.com>
2021-02-16 17:18:50 +0200

    Finished MainLayout

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/schedule', component: () => import('pages/Schedule.vue') },
+      { path: '/stats', component: () => import('pages/Statistics.vue') }
     ]
   },
 
