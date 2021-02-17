5fb3551e2e52f34a6ecb9a6eca6ce35c929ade8e
Mr.Stonkus <mr.valdas.stonkus@gmail.com>
2021-02-17 21:24:18 +0200

    Added two half cards

---
+++ src/components/RecommendationsCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <q-card class="home-card " bordered>
+      <q-card-section>
+        <div class="text-h6 ">Personal recommendations</div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+        <p class="q-mb-none">Keep using same weekly dose</p>
+        <p class="q-mb-none">Check INR after 10 days</p>
+      </q-card-section>
+    </q-card>
+
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="sass" scoped>
+
+.home-card
+  width: 100%
+  // max-width: 250px
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,44 +2,60 @@
   <div class="q-pa-md row items-start q-gutter-md text-dark">
 
     <StatusCard v-if="showStatusCard"/>
+    <RecommendationsCard v-if="showRecommendationsCard"/>
 
-    <q-card class="home-card " bordered>
+    <div class="card-width row ">
+      <div
+        class="col
+         q-card
+         q-mr-md
+         q-pa-md
+
+         "
+         >
+        INR check
+      </div>
+      <div
+        bordered
+        class="col
+         q-card
+         q-pa-md
+
+        ">Today warfarin
+      </div>
+
+    </div>
+
+    <q-card  bordered class="half-card">
       <q-card-section>
-        <div class="text-h6 ">Personal recommentations</div>
-      </q-card-section>
-      <q-card-section class="q-pt-none">
-        <p class="q-mb-none">Keep using same weekly dose</p>
-        <p class="q-mb-none">Check INR after 10 days</p>
+        <div class="text-h10">Next INR check</div>
+        <span class="text-subtitle2">Tap to check</span>
       </q-card-section>
     </q-card>
 
-    <q-card flat bordered class="my-card">
+    <q-card  bordered class="half-card">
       <q-card-section>
-        <div class="text-h6">How can i help myself?</div>
-        <div class="text-subtitle2">Stop smoking</div>
-        {{ lorem }}
+        <div class="text-h10">Next INR check</div>
+        <span class="text-subtitle2">Tap to check</span>
       </q-card-section>
-      <q-separator inset class="q-mb-sm"/>
-
-      <q-card-section class="q-pt-none">
-        <div class="text-subtitle2">More activity</div>
-        {{ lorem }}
-      </q-card-section>
-
     </q-card>
+
   </div>
 </template>
 
 <script>
 import StatusCard from '../components/StatusCard.vue'
+import RecommendationsCard from '../components/RecommendationsCard.vue'
 
 export default {
   components: {
-    StatusCard
+    StatusCard,
+    RecommendationsCard
   },
   data () {
     return {
       showStatusCard: true,
+      showRecommendationsCard: true,
       lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     }
   }
@@ -47,8 +63,11 @@ export default {
 </script>
 <style lang="sass" scoped>
 
-.home-card
+.card-width
   width: 100%
+
+.half-card
+  width: 45%
   // max-width: 250px
 
 </style>
