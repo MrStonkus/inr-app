f32d54ef4221c71670cdeea6cf9cdb27343a2130
Mr.Stonkus <mr.valdas.stonkus@gmail.com>
2021-02-18 17:29:24 +0200

    Add More and Less

--- src/components/StatusCard.vue
+++ src/components/StatusCard.vue
@@ -1,23 +1,57 @@
 <template>
     <q-card
+
       class="status-card"
       bordered
+      @click="expanded = !expanded"
       >
+
       <q-card-section>
         <div class="text-h6 text-center">Your status</div>
         <div class="text-h4 text-center text-positive text-bold">Very Good</div>
       </q-card-section>
-      <q-card-section class="q-pt-none">
+      <q-card-section class="q-py-none">
         <p class="q-mb-none">Current INR - 2.32</p>
         <p class="q-mb-none">Measurement was - today</p>
         <p class="q-mb-none">Weekly dose - 40mg</p>
         <p class="q-mb-none">Dose and medicine monitoring - automaticaly</p>
       </q-card-section>
+      <q-card-actions class="q-py-none">
+        <q-space />
+        <q-btn
+          color="grey"
+          class="text-capitalize"
+          flat
+          dense
+          :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          @click="expanded = expanded"
+          >{{moreOrLess}}
+        </q-btn>
+      </q-card-actions>
+      <q-slide-transition>
+        <div v-show="expanded">
+          <q-separator />
+          <q-card-section class="text-subitle1">
+            {{ lorem }}
+          </q-card-section>
+        </div>
+      </q-slide-transition>
+
     </q-card>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla hic voluptas ex in commodi, aliquid, doloribus ipsam tempora non quisquam numquam, pariatur id facilis repellendus atque perspiciatis nihil modi? Ab.',
+      expanded: false
+    }
+  },
+  computed: {
+    moreOrLess () {
+      return this.expanded ? 'Less' : 'More'
+    }
+  }
 }
 </script>
 <style lang="sass" scoped>
